f8a17af09cd30b99cdabd3f2aa03edb5aad13548
ukrainian-bird84 <143319836+ukrainian-bird84@users.noreply.github.com>
2024-05-16 23:09:40 +0200

    gkytc

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import { createFakeContact } from './utils/createFakeContact.js'; 
 import { PATH_DB } from '../constants/contacts.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    // Зчитування даних з файлу db.json
+    let dbData = [];
+    try {
+        const data = fs.readFileSync(PATH_DB, 'utf8');
+        dbData = JSON.parse(data);
+    } catch (err) {
+        console.error('Error reading db.json file:', err);
+    }
 
-await generateContacts(5);
+    // Генерування нових контактів та додавання їх до масиву
+    for (let i = 0; i < number; i++) {
+        const newContact = createFakeContact(); // коли функція createFakeContact створює новий контакт
+        dbData.push(newContact);
+    }
+
+    // Запис оновленого масиву контактів назад у файл db.json
+    try {
+        fs.writeFileSync(PATH_DB, JSON.stringify(dbData, null, 2));
+        console.log(`${number} contacts have been generated and added to db.json`);
+    } catch (err) {
+        console.error('Error writing to db.json file:', err);
+    }
+};
+
+export default generateContacts;
+
